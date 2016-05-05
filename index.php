1dceeb2c6f17b7aad5d0c39eb29bd754c9e0db29
Lucas Lauick <llaudick@cnm.edu>
2016-05-05 16:46:47 -0600

    added content

--- index.php
+++ index.php
@@ -32,5 +32,6 @@
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 		<title>Bootstrap Practice</title>
+
 	</head>
 </html>
